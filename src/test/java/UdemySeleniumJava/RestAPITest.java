66621d65269fe9d2298d9767eafca8723d5818d8
Himanshu Kumar <himanshu.kumar244@gmail.com>
2025-09-02 16:52:01 +0530

    Update RestAPITest.java

--- src/test/java/UdemySeleniumJava/RestAPITest.java
+++ src/test/java/UdemySeleniumJava/RestAPITest.java
@@ -1,23 +1,31 @@
+// Define the package where this class belongs
 package UdemySeleniumJava;
 
+// Import TestNG's @Test annotation for writing test cases
 import org.testng.annotations.Test;
 
+// Define the class RestAPITest
 public class RestAPITest {
 
-	
-	@Test
-	public void Demo4() {
-		System.out.println("Demo 4 A");
-	}	
-	
-	@Test
-	public void Demo6() {
-		System.out.println("Demo 6");
-	}
-	
-	@Test
-	public void Demo6A() {
-		System.out.println("New ");
-	}
-	
+    // First test case method, annotated with @Test
+    @Test
+    public void Demo4() {
+        // Print message to console when this test runs
+        System.out.println("Demo 4 A");
+    }   
+    
+    // Second test case method, annotated with @Test
+    @Test
+    public void Demo6() {
+        // Print message to console when this test runs
+        System.out.println("Demo 6");
+    }
+    
+    // Third test case method, annotated with @Test
+    @Test
+    public void Demo6A() {
+        // Print message to console when this test runs
+        System.out.println("New ");
+    }
+    
 }
