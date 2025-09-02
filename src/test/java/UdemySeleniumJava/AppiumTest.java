305b4556972a261498fb9e61b709b53382ad8e37
Himanshu Kumar <himanshu.kumar244@gmail.com>
2025-09-02 16:51:32 +0530

    Documentation updated for AppiumTest.java :pencil:

--- src/test/java/UdemySeleniumJava/AppiumTest.java
+++ src/test/java/UdemySeleniumJava/AppiumTest.java
@@ -1,19 +1,24 @@
+// Define the package where this class belongs
 package UdemySeleniumJava;
 
-import org.testng.annotations.Test;
+// Import TestNG's @Test annotation for writing test cases
 import org.testng.annotations.Test;
 
+// Define the class AppiumTest
 public class AppiumTest {
 
-	@Test
-	public void Demo1() {
-		System.out.println("Demo 1");
-	}
-	
-	@Test
-	public void Demo2() {
-		System.out.println("Demo 2");
-	}
-	
-	
+    // First test case method, annotated with @Test
+    @Test
+    public void Demo1() {
+        // Print message to console when this test runs
+        System.out.println("Demo 1");
+    }
+    
+    // Second test case method, annotated with @Test
+    @Test
+    public void Demo2() {
+        // Print message to console when this test runs
+        System.out.println("Demo 2");
+    }
+    
 }
